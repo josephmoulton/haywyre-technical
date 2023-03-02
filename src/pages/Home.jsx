29a35b4b096c509ab6b6cf82e7a8af96e202f15e
Joe <josephlewismoulton@gmail.com>
2023-03-02 10:35:58 +0000

    Home page complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import HomeImage from "../assets/undraw_dashboard_re_3b76.svg";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-        <Header></Header>
+      <Header></Header>
+      <div className="container">
+        <div className="top__conatiner">
+          <h1 className="page__title">
+            A website that issues CRUD commands on a REST API
+          </h1>
+          <h3 className="page__subtitle">
+            View our users to <span className="coloured">get started</span>
+          </h3>
+        </div>
+        <div className="button__container">
+          <button className="started__button">GET STARTED</button>
+        </div>
+        <div className="image__container">
+          <img className="home__image" src={HomeImage} alt="An image that shows a man with a chart" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
